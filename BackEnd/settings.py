ed6ad9fba5cdad1974216c269489e84956c5385b
muyanjarodney266 <muyanjarodney266@gmail.com>
2021-12-09 14:06:37 +0300

    secret key fix

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['markspredictorapi.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','markspredictorapi.herokuapp.com']
 
 
 # Application definition
