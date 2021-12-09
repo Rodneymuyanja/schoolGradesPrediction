7512a63ed12bfa8a398a022d67c06566f7d2f183
muyanjarodney266 <muyanjarodney266@gmail.com>
2021-12-09 11:15:55 +0300

    preparing for heroku deploy

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5@zbj90l@pgek+7y)ul^ir+hx=+7+77je&da4&8)*)l@ho8m+k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://markspredictorapi.herokuapp.com/']
 
 
 # Application definition
